57c2ae7b093b489b4f4606a8634381de8ab2a150
Taranjit Kaur <taranjitkaur.in@outlook.com>
2023-11-27 17:54:06 -0500

    Work In Progress - Corrected Sphinx Issues -Now, generates documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'Contact Manager'
 copyright = '2023, Taranjit Kaur'
@@ -14,7 +18,7 @@ release = 'v1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.todo','sphinx.ext.viewcode','sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
